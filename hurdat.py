6cb1d969345f83966b1e12a492f0fc6e45cfb282
wxmann <wxfreakk91@gmail.com>
2016-10-16 23:26:11 -0700

    Run plots and queries from command line.

--- hurdat.py
+++ hurdat.py
@@ -1,16 +1,52 @@
+import sys
 import load
-import visual
+import tcdata
 
 __author__ = 'tangz'
 
-# file = 'hurdat2-1851-2015-070616.txt'
+# __all__ = ['load_basin']
+#
+# _available_basins = """
+# \n Valid basins:
+# \n   * atlantic = ATLANTIC basin 1851-2015
+# """
+#
+#
+# def _log_error_message(msg):
+#     print('ERROR: ' + msg)
+#
+#
+# class _MissingBasinHook(tcdata.Queryable, tcdata.StormRetrievable):
+#     _ERROR_MESSAGE = \
+#         "Need to load data before running a query. Please execute load_basin(<basin_name>) first. " + _available_basins
+#
+#     def __init__(self):
+#         pass
+#
+#     def query(self, queryfunc):
+#         _log_error_message(_MissingBasinHook._ERROR_MESSAGE)
+#
+#     def get_tc(self, year, name=None, number=None):
+#         _log_error_message(_MissingBasinHook._ERROR_MESSAGE)
+#
+#
+wb = ()
+
+_ATLANTIC_FILE = 'data/hurdat2-1851-2015-070616.txt'
+#
+#
+# def load_basin(basin_str):
+#     global wb
+#     if basin_str.lower() == 'atlantic':
+#         wb = load.hurdat2(_ATLANTIC_FILE)
+#     else:
+#         _log_error_message('Basin: {0} not supported yet. {1}'.format(basin_str, _available_basins))
 
 
 def main():
-    matthew_storm = 'MATTHEW'
-    matthew = load.atcf('bal142016.dat', matthew_storm)
-    for datapoint in matthew:
-        print(datapoint)
+    global wb
+    if sys.argv[1] == '--atlantic':
+        wb = load.hurdat2(_ATLANTIC_FILE)
 
 
 if __name__ == '__main__':
--- visual.py
+++ visual.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
-from matplotlib import pylab
+
 import matplotlib.pyplot as plt
-import load
-import queries
 
 
 def _days_from_init(pt, t0):
@@ -11,34 +9,19 @@ def _days_from_init(pt, t0):
     return dt / timedelta(days=1)
 
 
-def plot_storm_windspeed(storm_to_plot, **kwargs):
-    time_pts = []
-    intensity_pts = []
-    t0 = storm_to_plot.first.timestamp
+def plot_storm_windspeed(storms_to_plot, **kwargs):
+    def _plot_single_storm(storm, **kwargs):
+        time_pts = []
+        intensity_pts = []
+        t0 = storm.first.timestamp
 
-    for datapoint in storm_to_plot:
-        time_pts.append(_days_from_init(datapoint, t0))
-        intensity_pts.append(datapoint.windspd)
+        for datapoint in storm:
+            time_pts.append(_days_from_init(datapoint, t0))
+            intensity_pts.append(datapoint.windspd)
 
-    plt.ylabel('Storm Intensity (kts)')
-    plt.xlabel('Number of days after being initially classified')
-    plt.plot(time_pts, intensity_pts, **kwargs)
+        plt.ylabel('Storm Intensity (kts)')
+        plt.xlabel('Number of days after being initially classified')
+        plt.plot(time_pts, intensity_pts, **kwargs)
 
-
-all_hurdat = load.hurdat2('hurdat2-1851-2015-070616.txt')
-first_year = 1960
-last_year = 2015
-last_decade = all_hurdat.query(queries.year_range(first_year, last_year))
-    
-#matthew_storm = 'MATTHEW'
-#matthew = load.atcf('bal142016.dat', matthew_storm)
-
-ivan = all_hurdat.get_tc(2004, 'IVAN')
-
-for storm in last_decade:
-    plot_storm_windspeed(storm.classifiable(), color='blue', alpha=0.1, linewidth=0.5)
-plot_storm_windspeed(ivan.classifiable(), color='red', linewidth=1)
-plt.axis([0, 16, 10, 160])
-plt.grid(True)
-plt.title('Hurricane Ivan (Red) vs. \nAll Atlantic TCs {0}-{1}'.format(first_year, last_year))
-pylab.savefig('ivan-{0}-{1}.png'.format(first_year, last_year), dpi=200, transparent=False)
+    for storm in storms_to_plot:
+        _plot_single_storm(storm, **kwargs)

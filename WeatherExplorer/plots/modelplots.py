4e7fcb1107f349a8074b88a19ebb6f7b23b24ebf
wxmann <wxfreakk91@gmail.com>
2016-10-23 01:58:56 -0700

    Interim commit: H5 height and vorticity plots.

--- WeatherExplorer/plots/modelplots.py
+++ WeatherExplorer/plots/modelplots.py
@@ -58,12 +58,17 @@ def to_hPa(pa):
     return pa * 0.01
 
 
+def gpm_to_dam(gpm):
+    return gpm * 0.1
+
+
 class ModelOutput(object):
     def __init__(self, modeldata, area):
         self.area = area
         self._data = modeldata
         self._lats = modeldata.variables['lat'][:]
         self._lons = modeldata.variables['lon'][:]
+        self._times = modeldata.variables['time'][:]
         self._fignum = 1
         self._plotted_figs = []
 
@@ -81,14 +86,15 @@ class ModelOutput(object):
 
     # the window parameter controls the number of highs and lows detected.
     # (higher value, fewer highs and lows)
-    def mslp(self, contour_intrv, title, window=20):
+    def mslp(self, countour_intrvl, title, hr=0, window=40):
         self._track_fig()
-        prmsl = to_hPa(self._data.variables['prmslmsl'][0])
+        index = hr/3
+        prmsl = to_hPa(self._data.variables['prmslmsl'][index])
 
         m = self.area.make_map()
         # add wrap-around point in longitude.
         prmsl, lons = addcyclic(prmsl, self._lons)
-        countour_lvls = np.arange(900, 1080., contour_intrv)
+        countour_lvls = np.arange(900, 1080., countour_intrvl)
         lons, lats = np.meshgrid(lons, self._lats)
         # adjust longitude point mismatch for negative values in map
         lons, prmsl = m.shiftdata(lons, datain=prmsl)
@@ -99,3 +105,39 @@ class ModelOutput(object):
         plt.title(title)
         plt.show()
         return self._fignum
+
+    def geoptnl_hgt(self, hr=0):
+        # self._track_fig()
+        index = hr/3
+        plotdata = gpm_to_dam(self._data.variables['hgtprs'][index][12])
+
+        m = self.area.make_map()
+        plotdata, lons = addcyclic(plotdata, self._lons)
+        countour_lvls = np.arange(462, 606, 6)
+        lons, lats = np.meshgrid(lons, self._lats)
+        # adjust longitude point mismatch for negative values in map
+        lons, plotdata = m.shiftdata(lons, datain=plotdata)
+        x, y = m(lons, lats)
+        CS = m.contour(x, y, plotdata, countour_lvls, colors='k', linewidths=1.)
+        plt.clabel(CS, countour_lvls, fontsize=10, fmt='%1.0f', inline_spacing=-2)
+        #
+        # plt.title(title)
+        # plt.show()
+        # return self._fignum
+
+    def absvort(self, hr=0):
+        # self._track_fig()
+        index = hr/3
+        plotdata = self._data.variables['absvprs'][index][12]
+
+        m = self.area.make_map()
+        plotdata, lons = addcyclic(plotdata, self._lons)
+        countour_lvls = np.arange(10e-5, 70e-5, 2e-5)
+        lons, lats = np.meshgrid(lons, self._lats)
+        # adjust longitude point mismatch for negative values in map
+        lons, plotdata = m.shiftdata(lons, datain=plotdata)
+        x, y = m(lons, lats)
+        m.contourf(x, y, plotdata, countour_lvls, cmap=plt.get_cmap('hot_r'))
+        #
+        # plt.show()
+        # return self._fignum
